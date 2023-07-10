6a37c287de4ca78e803a72c6b3519905fd7cbdf5
sudeshpariyar <sudesh.bullet@gmail.com>
2023-07-10 16:05:55 +0800

    Creating-header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Harvers App</header>
+    <div className="app">
+      <Header />
+      <span className="">Create New Room</span>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <span>Harvest Tech</span>
+    </div>
+  );
+};
